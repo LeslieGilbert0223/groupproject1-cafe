53eb5ee4c584bc423f61211f05e3abcf590e5d1b
lesliegilbert0223 <lesliegilbert0223@gmail.com>
2020-10-28 15:04:51 -0400

    Added the menu array

---
+++ script.js
@@ -0,0 +1,101 @@
+"use strict";
+
+let menu = [
+    {
+        name: "Soda",
+        category: "Drink",
+        description: "Coke Products",
+        price: 1.50,
+    }
+    {
+        name: "Coffee",
+        category: "Drink",
+        description: "Regular or Decaf",
+        price: 2.00,
+    }
+    {
+        name: "Espresso",
+        category: "Drink",
+        description: "By the shot",
+        price: 1.50,
+    }
+    {
+        name: "Tea",
+        category: "Drink",
+        description: "Green or Black",
+        price: 1.00,
+    }
+    {
+        name: "Club",
+        category: "Sandwich",
+        description: "Turkey, bacon, lettuce, tomato, american cheese, and black garlic mayonnaise.",
+        price: 7.50,
+    }
+    {
+        name: "Corned Beef",
+        category: "Sandwich",
+        description: "Corned beef with swiss cheese on grilled rye",
+        price: 6.50,
+    }
+    {
+        name: "Tuna Melt",
+        category: "Sandwich",
+        description: "Tuna salad with American cheese on homemade grilled rye",
+        price: 5.50,
+    }
+    {
+        name: "BLT",
+        category: "Sandwich",
+        description: "Bacon, lettuce, tomato and mayonnaise on white toast",
+        price: 4.50,
+    }
+    {
+        name: "French Baguette",
+        category: "Pastry",
+        description: "Warm and fresh from the oven",
+        price: 1.50,
+    }
+    {
+        name: "Croissants",
+        category: "Pastry",
+        description: "Buttery puff pastry, perfect for breakfast!",
+        price: 2.00,
+    }
+    {
+        name: "Danish",
+        category: "Pastry",
+        description: "Raspberry filled with icing",
+        price: 2.00,
+    }
+    {
+        name: "Donut",
+        category: "Pastry",
+        description: "Iced cake-style",
+        price: 1.50,
+    }
+    {
+        name: "Chocolate Cake",
+        category: "Dessert",
+        description: "A rich velvety chocolate genoise sponge with a dark chocolate ganache, covered with a delicate strawberry sauce",
+        price: 3.00,
+    }
+    {
+        name: "Ice Cream Sundae",
+        category: "Dessert",
+        description: "True Vanilla Bean Ice Cream served with a chocolate ganache and crunchy peanut topping",
+        price: 2.50,
+    }
+    {
+        name: "Toffee Almond Bar",
+        category: "Dessert",
+        description: "The best damn dessert you’ll ever have. Guaranteed",
+        price: 1.75,
+    }
+    {
+        name: "Carrot Cake",
+        category: "Dessert",
+        description: "Carrot cake is good, actually!",
+        price: 3.00,
+    }
+];
+console.log(menu);
